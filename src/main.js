1e317fa3920838c2bcd71c9799cbb50479abb591
Ricardo Conceição <ricardo220893@gmail.com>
2019-08-02 23:28:33 -0300

    - Creating smoke tests.

---
+++ src/main.js
@@ -0,0 +1,13 @@
+const search = () => {};
+const searchAlbums = () => {};
+const searchArtists = () => {};
+const searchTracks = () => {};
+const searchPlaylists = () => {};
+
+export {
+  search,
+  searchAlbums,
+  searchArtists,
+  searchTracks,
+  searchPlaylists,
+};
---
+++ tests/main.spec.js
@@ -0,0 +1,28 @@
+import { expect } from 'chai';
+import {
+  search, searchAlbums, searchArtists, searchPlaylists, searchTracks,
+} from '../src/main';
+
+describe('Spotify Wrapper', () => {
+  describe('Smoke Tests', () => {
+    it('should exist the search method', () => {
+      expect(search).to.exist;
+    });
+
+    it('should exist the searchAlbums method', () => {
+      expect(searchAlbums).to.exist;
+    });
+
+    it('should exist the searchArtists method', () => {
+      expect(searchArtists).to.exist;
+    });
+
+    it('should exist the searchTracks method', () => {
+      expect(searchTracks).to.exist;
+    });
+
+    it('should exist the searchPlaylists method', () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
+});
